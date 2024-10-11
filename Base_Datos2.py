fb09ad82d9173ed2dd61f0965e963bbaa7a18e61
azrrael22 <adriaannm.rodriguezr@uqvirtual.edu.co>
2024-10-10 23:31:21 -0500

    Quinto comit del proyecto, modificacion del codigo para generar la base de datos para las nuevas variables bibliograficas

--- Base_Datos2.py
+++ Base_Datos2.py
@@ -3,20 +3,26 @@ import sqlite3
 import re
 
 # Rutas de los archivos CSV
-rutaIEEE = 'Archivos_CSV/archivo_combinado.csv'
-rutaScience = 'Archivos_CSV/archivo_combinadoScienceD.csv'
-rutaScopus = 'Archivos_CSV/scopus solo me dejo 20000.csv'
+rutaIEEE = 'Archivos/archivo_combinadoIEEE.csv'
+rutaScience = 'Archivos/archivo_combinadoScienceDirect.csv'
+rutaScopus = 'Archivos/scopus.csv'
+rutaSage = 'Archivos/archivo_combinadoSage.csv'
+rutaTF = 'Archivos/archivo_combinadoTF.csv'
 
 # Leer los CSV
 df1 = pd.read_csv(rutaIEEE) 
 df2 = pd.read_csv(rutaScience) 
 df3 = pd.read_csv(rutaScopus) 
+df4 = pd.read_csv(rutaSage)
+df5 = pd.read_csv(rutaTF)
 
 # Asociar cada DataFrame con su base de datos correspondiente
 dfList = [
     (df1, 'IEEE'),
     (df2, 'ScienceDirect'),
-    (df3, 'Scopus')
+    (df3, 'Scopus'),
+    (df4, 'Sage'),
+    (df5, 'Taylor and Francis')
 ]
 
 # Conectar a la base de datos (se creará si no existe)
@@ -29,15 +35,15 @@ CREATE TABLE IF NOT EXISTS publicaciones (
     id_publicacion INTEGER PRIMARY KEY AUTOINCREMENT,
     titulo TEXT NOT NULL UNIQUE,
     primer_autor TEXT,
-    afiliacion_primer_autor TEXT,
-    anio_publicacion INTEGER,
+    anio_publicacion INTEGER DEFAULT 0,
     tipo_producto TEXT,
+    paginas INTEGER,
     journal TEXT,
-    publisher TEXT,
+    volumen INTEGER DEFAULT 0,
+    issue INTEGER DEFAULT 0,
     base_datos TEXT,
-    cantidad_citaciones INTEGER DEFAULT 0,
     resumen TEXT,
-    palabras_clave TEXT,
+    url TEXT,
     doi TEXT
 );
 ''')
@@ -51,19 +57,14 @@ for df, nombre_base in dfList:
     for index, row in df.iterrows():
         titulo = row.get('Document Title') or row.get('title') or row.get('Title')
         autores = row.get('Authors') or row.get('author')
-        afiliaciones = row.get('Author Affiliations') or row.get('Affiliations')
         anio_publicacion = row.get('Publication Year') or row.get('year') or row.get('Year')
-        tipo_producto = row.get('Document Identifier') or row.get('Document Type')
+        tipo_producto = row.get('Document Identifier') or row.get('Document Type') or row.get('ENTRYTYPE')
         journal = row.get('Publication Title') or row.get('journal') or row.get('Source title')
-        publisher = row.get('Publisher')
         base_datos = nombre_base  # Asignar el nombre de la base de datos
-        cantidad_citaciones = row.get('Article Citation Count') or row.get('Cited by') or 0
         resumen = row.get('Abstract') or row.get('abstract')
-        palabras_clave = row.get('Author Keywords') or row.get('keywords')
         doi = row.get('DOI') or row.get('doi')
 
         # Asegurarse de que los campos numéricos sean del tipo correcto
-        cantidad_citaciones = int(cantidad_citaciones) if pd.notnull(cantidad_citaciones) else 0
         anio_publicacion = int(anio_publicacion) if pd.notnull(anio_publicacion) else None
 
         # Extraer el primer autor
@@ -72,13 +73,6 @@ for df, nombre_base in dfList:
             primer_autor = lista_autores[0] if lista_autores else None
         else:
             primer_autor = None
-
-        # Extraer la afiliación del primer autor
-        if pd.notnull(afiliaciones):
-            lista_afiliaciones = [afiliacion.strip() for afiliacion in re.split(';', afiliaciones, flags=re.IGNORECASE)]
-            afiliacion_primer_autor = lista_afiliaciones[0] if lista_afiliaciones else None
-        else:
-            afiliacion_primer_autor = None
 
         # Limpiar el campo 'tipo_producto' para eliminar 'IEEE' si está presente
         if pd.notnull(tipo_producto) and isinstance(tipo_producto, str):
@@ -89,22 +83,22 @@ for df, nombre_base in dfList:
             try:
                 cursor.execute('''
                 INSERT INTO publicaciones (
-                    titulo, primer_autor, afiliacion_primer_autor, anio_publicacion,
-                    tipo_producto, journal, publisher, base_datos,
-                    cantidad_citaciones, resumen, palabras_clave, doi
+                    titulo, primer_autor, anio_publicacion,
+                    tipo_producto, paginas, journal, volumen, issue, base_datos,
+                    resumen, url, doi
                 ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
                 ''', (
                     titulo,
                     primer_autor,
-                    afiliacion_primer_autor,
                     anio_publicacion,
                     tipo_producto,
+                    paginas,
                     journal,
-                    publisher,
+                    volumen,
+                    issue,
                     base_datos,
-                    cantidad_citaciones,
                     resumen,
-                    palabras_clave,
+                    url,
                     doi
                 ))
             except sqlite3.IntegrityError:
